9ed9a4c3b683754da1334fb7aec5cd14ea89e6ad
Shaul Kahane <35196468+shaolkahna@users.noreply.github.com>
2023-02-05 14:45:56 +0200

    insert is working

--- main_sql_input.ts
+++ main_sql_input.ts
@@ -2,8 +2,7 @@ import * as readline from "node:readline/promises";
 import {stdin as input, stdout as output } from "node:process";
 import{User_line}from "./interfces"
 import {insert_id_to_map} from './DB_model/Read_file';
-import {add_user_input,search_user_input,delete_user_input} from "./user_IU/input"
-
+import {insertUser } from './user_IU/check_sql_form'
 
 let user_line:User_line = new User_line;
 const user_map = new Map();
@@ -22,19 +21,19 @@ async function db_maneger() {
 
     inputQ = await rl.question("");
 
-    action = inputQ.split(" ")[0]
+    action = inputQ.trim().split(" ")[0].trim().toLowerCase()
 
     switch (action) {
-        case "select" || "SELECT":
-            add_user_input(user_line,user_map).then(() => {db_maneger(); })
+        case "select":
+            //add_user_input(user_line,user_map).then(() => {db_maneger(); })
             break;
 
-        case "insert" || "INSERT":
-            search_user_input(user_map).then(() => {db_maneger(); })
+        case "insert":
+            insertUser(inputQ,user_line,user_map).then(() => {db_maneger(); })
             break;
 
-        case "delete" || "DELETE":
-            delete_user_input(user_map).then(() => {db_maneger(); })
+        case "delete":
+            //delete_user_input(user_map).then(() => {db_maneger(); })
             break;
 
         case "exit":
--- user_IU/check_sql_form.ts
+++ user_IU/check_sql_form.ts
@@ -1,8 +1,11 @@
 
 import { check } from "./check_input";
 import { questions } from "../questions_list";
+import{User_line}from "../interfces"
+import{addUser}from '../DB_model/add_User';
 
-function insertUser(insert_string:string){
+
+export async function insertUser(insert_string:string,user_line:User_line,user_map:Map<string,number>){
 
     if(!insert_string.includes(" values ") || insert_string.slice(0,Math.min(insert_string.indexOf("("),insert_string.indexOf("values"))).trim().toUpperCase()!== "INSERT INTO FILE")
         console.log("go to ell")
@@ -10,6 +13,8 @@ function insertUser(insert_string:string){
         insert_string = insert_string.trim().slice(17).trim()
         let insert_array:Array<string> = insert_string.split("values")
         const answers: Array<string> = [];
+        let id:string = ""
+
         if (insert_array[0].trim() == "") {
 
             let values:Array<string> = cleer(insert_array[1].trim())
@@ -17,12 +22,20 @@ function insertUser(insert_string:string){
             for (let v of values) {
                 let answer:string|void = check(questions[indexs].type, v, questions[indexs].length);
                 if (answer) {
+                    if (questions[indexs].name === "id: ") {
+                        id = answer
+                        if (user_map.get(id)!== undefined) {
+                            console.log("The user already exist in the DB");
+                            return
+                        }
+                    }
                     answers.push(questions[indexs].name + answer.padEnd(questions[indexs].length));
                 }
                 indexs ++
             }
             let answersString:string = answers.toString() 
-            console.log(answersString);
+            addUser(answersString,id,user_line,user_map)
+            //console.log(answersString);
 
         } else {
             let colomes:Array<string> = cleer(insert_array[0].trim())
@@ -33,7 +46,17 @@ function insertUser(insert_string:string){
                     if (colomes.includes(new_name)){
                         let answer:string|void = check(question.type, values[colomes.indexOf(new_name)], question.length);
                         if (answer) {
+                            if (question.name === "id: ") {
+                                id = answer
+                                if (user_map.get(id)!== undefined) {
+                                    console.log("The user already exist in the DB");
+                                    return
+                                }
+                            }
                             answers.push(question.name + answer.padEnd(question.length));
+                        }
+                        else{
+                            return
                         }
                     }
                     else{
@@ -42,7 +65,7 @@ function insertUser(insert_string:string){
                     }
                 }
                 let answersString:string = answers.toString() 
-                console.log(answersString);           
+                addUser(answersString,id,user_line,user_map)
             }else{
                 console.log("Most to insert a id")
             }
@@ -62,3 +85,6 @@ function cleer(input:string):Array<string>{
 }
 
 
+let a = " insert into file   values ( 123456789  , yaki , klein, 43, israel, jerusalem, jontan, 22, Male, 3  )"
+let k = " INSERT INTO file    ( First name , id , Family name ) values (yaki , 123111119, klein ) "
+let r = "insert into file (id, first_name, last_name) values (1234, yaki, klein)"
