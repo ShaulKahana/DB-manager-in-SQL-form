f703f015e19d518e3f590e191f12340c785e05ed
Shaul Kahane <35196468+shaolkahna@users.noreply.github.com>
2023-02-08 11:13:18 +0200

    main file change

--- main_sql_input.ts
+++ main_sql_input.ts
@@ -6,44 +6,49 @@ import {insertUser } from './user_IU/check_sql_insert_form'
 import {selectUser } from './user_IU/check_sql_select_form'
 
 let user_line:User_line = new User_line;
-const user_map = new Map();
+const user_map:Map<string,number> = new Map();
+let flag:boolean = true
 
 
 
+async function db_maneger():Promise<void> {
+    const rl:readline.Interface = readline.createInterface({ input, output, terminal: false });
 
-async function db_maneger() {
+    while(flag){
 
-    const rl = readline.createInterface({ input, output, terminal: false });
-    let input_query:string ;
-    let action:string ;
+        let input_query:string ;
+        let action:string ;
 
-    input_query = await rl.question("Hello and welcome, enter your SQL query\n");
+        input_query = await rl.question("Hello and welcome, enter your SQL query\n");
 
-    action = input_query.trim().split(" ")[0].trim().toLowerCase()
+        action = input_query.trim().split(" ")[0].trim().toLowerCase()
 
-    switch (action) {
-        case "insert":
-            insertUser(input_query,user_line,user_map).then(() => {db_maneger(); })
-            break;
+        switch (action) {
+            case "insert":
+                await insertUser(input_query,user_line,user_map)
+                break;
 
-        case "select":
-            selectUser(input_query,user_map).then(() => {db_maneger(); })
-            break;
+            case "select":
+                await selectUser(input_query,user_map)
+                break;
 
-        case "delete":
-            //delete_user_input(user_map).then(() => {db_maneger(); })
-            break;
+            case "delete":
+                //delete_user_input(user_map).then(() => {db_maneger(); })
+                break;
 
-        case "exit":
-            console.log("Have a good day");
-            rl.close();
-            process.exit()
+            case "exit":
+                console.log("Have a good day");
+                rl.close();
+                flag = false
+                break;
 
-        default:
-            console.error("The SQL query must begin wite 'select' or 'insert' or 'delete', or press 'exit' to finish!\n");
-            db_maneger()
-            break;
+            default:
+                console.error("The SQL query must begin wite 'select' or 'insert' or 'delete', or press 'exit' to finish!\n");
+                break;
+        }
+
     }
+
 }
 
 
